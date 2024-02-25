787b0b66c7e87293b4b6e458b35cb1d15c7c1a3e
Ronald Prasad <dlanordasarp221@gmail.com>
2024-02-25 15:06:58 -0800

    Sign into existing account authentication

--- bites/budget_bites/lib/services/auth.dart
+++ bites/budget_bites/lib/services/auth.dart
@@ -5,11 +5,29 @@ class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
   // sign in with email and password
+  Future signIn(String email, String pass) async {
+    try {
+      final UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: pass
+      );
+      User? user = userCredential.user;
+      print("Found user: ${user!.uid}");
+      return user.uid;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      }
+      return false;
+    }
+  }
 
   // register with email and password
   Future registerWEP(String email, String password) async {
     try {
-      UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+      UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
         email: email,
         password: password,
       );
